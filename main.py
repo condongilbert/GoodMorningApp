3a459af2b1595c1a3359df03715df5b1d1faf918
Gilbert <condongilbert@gmail.com>
2024-10-17 11:08:24 -0700

    modified function names

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from image_generator import create_good_morning_image
+from image_generator import create_simple_image
 from email_sender import send_email
 
 # Generate the image
-create_good_morning_image()
+create_simple_image()
 
 # Send the image via email
 send_email()
