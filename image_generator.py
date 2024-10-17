d4870d144ed59aa006f026e7efdf8644f48022e8
Gilbert <condongilbert@gmail.com>
2024-10-17 08:35:30 -0700

    image_generator.py file now generator image with text

--- image_generator.py
+++ image_generator.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import os
 
 # Print current working directory
@@ -9,8 +9,16 @@ def create_simple_image():
     img = Image.new('RGB', (800, 600), color=(255, 223, 186))
     draw = ImageDraw.Draw(img)
 
-    # Draw a simple rectangle or text
-    draw.rectangle([50, 50, 750, 550], outline="purple", width=5)
+    message = "Good Morning, Beautiful!"
+    font = ImageFont.truetype("arial.ttf", 40)
+    text_bbox = draw.textbbox((0, 0), message, font=font)
+    text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
+    
+    # Calculate the position to center the text
+    position = ((img.width - text_width) // 2, (img.height - text_height) // 2)
+    
+    # Draw the text
+    draw.text(position, message, fill=(139, 0, 139), font=font)
     
     # Save the image in the same directory as the script
     save_path = "simple_image.png"
